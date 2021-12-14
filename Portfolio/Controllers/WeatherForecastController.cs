763447a5b3f94f93547b8d50375574cfb11f094c
levvy13 <mlewandowski1994@wp.pl>
2021-12-14 22:08:12 +0100

    test

--- Portfolio/Controllers/WeatherForecastController.cs
+++ Portfolio/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Portfolio.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "test", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
